3d7cf1ae391b4d2c0736c21fa7ef45791fb02a8a
Dodley <dodleydesign+github@gmail.com>
2023-10-15 22:02:19 +0100

    Small changes to server and seeding

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -2,13 +2,13 @@ import { PrismaClient } from "@prisma/client";
 import { faker } from "@faker-js/faker";
 const prisma = new PrismaClient();
 async function main() {
-  prisma.product.deleteMany();
+  await prisma.product.deleteMany();
   for (let i = 0; i <= 500; i++) {
     await prisma.product.create({
       data: {
         title: faker.commerce.product(),
         amount: Number(faker.commerce.price({ min: 20, max: 1000 })),
-        imageUrl: `picsum.photos/${700 - i}/200`,
+        imageUrl: `https://picsum.photos/${700 - i}/200`,
         currency: faker.finance.currencySymbol(),
       },
     });
