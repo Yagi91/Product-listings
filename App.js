cd9817ce3c2c655b036fbb63546d1c9312d06f32
Dodley <dodleydesign+github@gmail.com>
2023-10-15 18:17:07 +0100

    Added ApolloClient to make backend requests

--- App.js
+++ App.js
@@ -1,90 +1,50 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  useQuery,
+  gql,
+} from "@apollo/client";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Header from "./components/Header";
 import SearchBar from "./components/searchBar";
+import client from "./client";
 import ProductListings from "./components/ProductListings";
 
 export default function App() {
-  const products = [
-    {
-      id: 1,
-      name: "Product 1",
-      imageUrl: "https://picsum.photos/151",
-      amount: 10.99,
-      currency: "USD",
-    },
-    {
-      id: 2,
-      name: "Product 2",
-      imageUrl: "https://via.placeholder.com/150",
-      amount: 20.99,
-      currency: "USD",
-    },
-    {
-      id: 3,
-      name: "Product 3",
-      imageUrl: "https://via.placeholder.com/150",
-      amount: 30.99,
-      currency: "USD",
-    },
-    {
-      id: 4,
-      name: "Product 4",
-      imageUrl: "https://via.placeholder.com/150",
-      amount: 40.99,
-      currency: "USD",
-    },
-    {
-      id: 5,
-      name: "Product 5",
-      imageUrl: "https://via.placeholder.com/150",
-      amount: 50.99,
-      currency: "USD",
-    },
-    {
-      id: 6,
-      name: "Product 6",
-      imageUrl: "https://via.placeholder.com/150",
-      amount: 60.99,
-      currency: "USD",
-    },
-    {
-      id: 7,
-      name: "Product 7",
-      imageUrl: "https://via.placeholder.com/150",
-      amount: 70.99,
-      currency: "USD",
-    },
-    {
-      id: 8,
-      name: "Product 8",
-      imageUrl: "https://via.placeholder.com/150",
-      amount: 80.99,
-      currency: "USD",
-    },
-    {
-      id: 9,
-      name: "Product 9",
-      imageUrl: "https://via.placeholder.com/150",
-      amount: 90.99,
-      currency: "USD",
-    },
-    {
-      id: 10,
-      name: "Product 10",
-      imageUrl: "https://via.placeholder.com/150",
-      amount: 100.99,
-      currency: "USD",
-    },
-  ];
-
+  {}
+  // if (loading) {
+  //   return (
+  //     <View>
+  //       <Text>Loading...</Text>
+  //     </View>
+  //   );
+  // }
+  // if (error) {
+  //   return <View>
+  //     <Text>Error loading products</Text>
+  //   </View>
+  // }
   return (
-    <View style={styles.container}>
-      <Header />
-      <SearchBar />
-      <ProductListings products={products} />
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <View style={styles.container}>
+        <Header />
+        <SearchBar />
+        <ProductListings
+          products={[
+            {
+              id: 1,
+              imageUrl: "picsum.photos/200/300",
+              amount: "500",
+              currency: "USD",
+              name: "Test product",
+            },
+          ]}
+        />
+        <StatusBar style="auto" />
+      </View>
+    </ApolloProvider>
   );
 }
 
--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
 
-//ProductCard component should contain a name, an image, amount  and currency.
+//ProductCard component should contain a title, an image, amount and currency.
 
-export default function ProductCard({ name, image, amount, currency }) {
+export default function ProductCard({ title, imageUrl, amount, currency }) {
   return (
     <TouchableOpacity style={styles.productCard}>
-      <Image style={styles.productCardImage} source={{ uri: image }} />
-      <Text style={styles.productCardText}>{name}</Text>
+      <Image style={styles.productCardImage} source={{ uri: imageUrl }} />
+      <Text style={styles.productCardText}>{title}</Text>
       <Text style={styles.productCardText}>
         {currency} {amount}
       </Text>
